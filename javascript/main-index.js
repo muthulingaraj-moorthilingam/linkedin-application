0d4b362280534576a4a82e473578acb1656482e2
muthulingaraj moorthilingam <muthutamizha.mp96@gmail.com>
2020-05-07 23:55:49 +0530

    edit

--- javascript/main-index.js
+++ javascript/main-index.js
@@ -5,6 +5,7 @@ var model_post = new UserPostdata(model_data);
 //console.log("TEST", model_post.getData());
 if(model_post){
 	fetchData();
+
 }
 else{
 	console.error("Object not created")
@@ -16,10 +17,19 @@ const _view_post = new RenderView(parentElement);
 
 const _controller_post = new ControllerPost(model_post,_view_post);
 
-async function fetchData(){
- 	console.log("TEST",await model_post.getData());
- 	console.log(await model_post);
- 	_controller_post.renderPost();
+function fetchData(){
+ 	//console.log("TEST", model_post.getData());
+ 	//console.log( model_post);
+ 	//_controller_post.renderPost();
+ 	model_post.getData()
+ 	.then(function(data){
+ 		console.log('Fetch',data)
+ 		console.log( model_post);
+ 		_controller_post.renderPost();
+ 	})
+ 	.catch(function(error){
+ 		console.log(error)
+ 	});
  }
 
  //_controller_post.renderPost();
--- models/post-model.js
+++ models/post-model.js
@@ -5,8 +5,18 @@ function UserPostdata(model_data){
 
 UserPostdata.prototype.getData = async function(){
 	if(this.data === undefined){
-		await apiCall();
-		return this.data;
+		//apiCall();
+		try{
+			let response = await fetch(`https://testapi.io/api/muthulingaraj/https://testapi.io/api/muthulingaraj/linkedin/user/post`);
+    		let data = await response.json();
+    		 model_post.setData(data);
+    		return data;
+		}   
+		catch(err){
+			console.log(err)
+			return err;
+		}
+		//return this.data;
 	}
 	else{
 		return this.data;
@@ -16,14 +26,43 @@ UserPostdata.prototype.getData = async function(){
 UserPostdata.prototype.setData = function(value){
 	this.data=value;
 };
-
+/*
 async function apiCall(){
-	let response = await fetch(`https://testapi.io/api/muthulingaraj/https://testapi.io/api/muthulingaraj/linkedin/user/post`);
-    let data = await response.json();
-    model_post.setData(data);
+	try{
+		let response = await fetch(`https://testapi.io/api/muthulingaraj/https://testapi.io/api/muthulingaraj/linkedin/user/post`);
+    	let data = await response.json();
+    	model_post.setData(data);
+	}   
+	catch(err){
+		console.log(err)
+	}
+	//return data;
 }
-
-
+/*
+function apiCall(url){
+	return new Promise(function(resolve,reject){
+		/*var httpRequest = new XMLHttpRequest();
+		httpRequest.open("GET",url);
+		httpRequest.send();
+		httpRequest.onload=function(){
+			if(httpRequest.status === 200){
+				resolve(httpRequest.response)
+			}
+			else{
+				reject(Error(httpRequest.statusText));
+			}
+		}
+		fetch(url)
+		.then(res => {
+			//return res.json();
+			resolve(res.json())
+		})
+		//.then(data => {
+		//	resolve(data);
+		//})
+	});
+}
+*/
 
 
 
